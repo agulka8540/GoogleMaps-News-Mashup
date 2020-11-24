ca8d201ac17291e22ac841f5fc598ec46627bd36
AgnieszkaF <agulka8540@googlemail.com>
2020-11-24 19:11:35 +0100

    initial commit

---
+++ public/articles.php
@@ -0,0 +1,21 @@
+<?php
+
+    require(__DIR__ . "/../includes/config.php");
+
+    // ensure proper usage
+    if (empty($_GET["geo"]))
+    {
+        http_response_code(400);
+        exit;
+    }
+
+    // get geography's articles
+    $articles = lookup($_GET["geo"]);
+    $articles = array_slice($articles, 1, 5);
+
+
+    // output articles as JSON (pretty-printed for debugging convenience)
+    header("Content-type: application/json");
+    print(json_encode($articles, JSON_PRETTY_PRINT));
+
+?>
---
+++ public/search.php
@@ -0,0 +1,20 @@
+<?php
+
+    require(__DIR__ . "/../includes/config.php");
+
+    // numerically indexed array of places
+    $places = [];
+
+    // TODO: search database for places matching $_GET["geo"], store in $places
+    $places = CS50::query(
+        "SELECT * FROM places WHERE ".
+        "postal_code LIKE ? OR ".
+        "CONCAT(place_name, ', ', admin_name1) LIKE ? OR ".
+        "CONCAT(place_name, ', ', admin_code1) LIKE ? LIMIT 10",
+        $_GET["geo"] . "%", $_GET["geo"] . "%", $_GET["geo"] . "%");
+
+    // output places as JSON (pretty-printed for debugging convenience)
+    header("Content-type: application/json");
+    print(json_encode($places, JSON_PRETTY_PRINT));
+
+?>
